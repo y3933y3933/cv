116b42a8bec59c68331d64f16dae98a8eec8d5fa
Joanne Lin <y3933y3933@gmail.com>
2024-09-22 18:47:19 +0800

    feat: update cv

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,15 +20,15 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "林蕎安",
+  name: "Chiao-An Lin",
   initials: "Joanne",
-  location: "台北市, 臺灣",
+  location: "Taipei, Taiwan",
   locationLink: "https://maps.app.goo.gl/xEAVZEACqQwGyaV77",
-  about: "前端工程師",
+  about: "Frontend Developer",
   summary:
-    "擔任前端工程師 4 年，擁有從 0 到 1 建置前端專案經驗，熟悉React/Next、Angular、TypeScript、Tailwind CSS。\n文件寫手與技術潮流追求者，近期愛用工具是 Cursor Code Editor 、 Supermaven 與 Arc Browser。",
+    "4 years of experience as a frontend developer, proficient in building frontend projects from scratch. Skilled in React/Next, Angular, TypeScript, and Tailwind CSS.",
   avatarUrl: "",
-  personalWebsiteUrl: "https://y3933y3933.github.io/",
+  personalWebsiteUrl: "https://blog-flax-five.vercel.app/",
   contact: {
     email: "y3933y3933@gmail.com",
     tel: "+886 9 1658 8526",
@@ -47,30 +47,30 @@ export const RESUME_DATA = {
   },
   education: [
     {
-      school: "國立臺灣大學 National Taiwan University",
-      degree: "心理系學士 Bachelor's Degrees in Psychology",
+      school: "National Taiwan University",
+      degree: "Bachelor's Degrees in Psychology",
       start: "2016",
       end: "2020",
     },
   ],
   work: [
     {
-      company: "萬通教育 OneClass",
+      company: "OneClass",
       link: "https://tutor.oneclass.com.tw/",
       badges: [],
       title: "Senior Frontend Developer",
-      logo: ParabolLogo,
+      // logo: ParabolLogo,
       start: "2023",
-      end: "至今",
+      end: "Present",
       description: [
-        "從 0 到 1 建置 Next 專案，使用 <span style='color: rgb(217 119 6); font-weight: semibold;'>Tailwind CSS</span> 與 <span style='color: rgb(217 119 6); font-weight: semibold;'> Shadcn/ui </span> 加速切版與建置 UI 元件、透過 <span style='color: rgb(217 119 6); font-weight: semibold;'>Zod</span> 與 <span style='color: rgb(217 119 6); font-weight: semibold;'>React-hook-form</span> 簡化表單檢核流程、使用 <span style='color: rgb(217 119 6); font-weight: semibold;'>SWR</span> 優化頁面資料讀取。",
-        ,
-        "維護與重構 <span style='color: rgb(217 119 6); font-weight: semibold;'>React</span> 後台專案、並使用 <span style='color: rgb(217 119 6); font-weight: semibold;'>React Query</span> 串接 API。",
-        "推動團隊撰寫工程技術文件、主動分享技術新知與學習資源、擔任Code Review角色。",
+        "Spearheaded the development of a Next.js project from inception, leveraging Tailwind CSS and Shadcn/ui to accelerate UI component creation and streamline the design process.",
+        "Led the maintenance and refactoring of a React-based admin dashboard, improving performance and code maintainability.",
+        "Championing the creation and maintenance of comprehensive technical documentation",
+        "Mentoring junior engineers, facilitating their professional growth and skill development",
       ],
     },
     {
-      company: "多奇數位 Duotify",
+      company: "Duotify",
       link: "https://www.duotify.com/",
       badges: ["Remote"],
       title: "Frontend Developer",
@@ -78,12 +78,12 @@ export const RESUME_DATA = {
       start: "2021",
       end: "2023",
       description: [
-        "從 0 到 1 建置  <span style='color: rgb(217 119 6); font-weight: semibold;'>Angular 15+</span> 專案，使用 <span style='color: rgb(217 119 6); font-weight: semibold;'>Nx</span> 架構開發政府基金資訊前後台，透過 <span style='color: rgb(217 119 6); font-weight: semibold;'> Angular CDK </span>、<span style='color: rgb(217 119 6); font-weight: semibold;'> Angular Material </span> 與 <span style='color: rgb(217 119 6); font-weight: semibold;'> Tailwind CSS </span> 開發 UI 元件、採用<span style='color: rgb(217 119 6); font-weight: semibold;'> Openapi-generator </span>工具自動生成 API Service。",
-        "擔任前端技術窗口，與客戶、設計、後端廠商協作，討論前後端規格與評估需求可行性，使用 <span style='color: rgb(217 119 6); font-weight: semibold;'>Angular</span> 開發基富通前台頁面，整合第三方服務，如：<span style='color: rgb(217 119 6); font-weight: semibold;'> Google Analytics </span> 服務、<span style='color: rgb(217 119 6); font-weight: semibold;'> Highchart.js </span> 開發基金圖表。",
+        "Led the development of an Angular 15+ project using Nx architecture for a government fund information platform.",
+        "Acted as the frontend technical liaison, collaborating with stakeholders to define specifications and assess feasibility.",
       ],
     },
     {
-      company: "昕力資訊 TPIsoftware",
+      company: "TPIsoftware",
       link: "https://www.tpisoftware.com/",
       badges: [],
       title: "Frontend Developer",
@@ -91,9 +91,9 @@ export const RESUME_DATA = {
       start: "2020",
       end: "2021",
       description: [
-        "使用 <span style='color: rgb(217 119 6); font-weight: semibold;'> Angular </span> 開發銀行後台系統",
-        " <span style='color: rgb(217 119 6); font-weight: semibold;'>Vue2+</span> 開發公司內部系統。",
-        "擔任 <span style='color: rgb(217 119 6); font-weight: semibold;'>Electron</span> 技術分享主講者。",
+        "Built internal company systems with Vue 2+.",
+        "Developed a bank’s back-office system using Angular.",
+        "Served as the presenter for Electron technology sharing sessions, fostering knowledge exchange within the team.",
       ],
     },
   ],
@@ -120,7 +120,7 @@ export const RESUME_DATA = {
         "React hook form",
       ],
       description:
-        "國中小線上家教課程介紹與購買平台，使用者可以登入/註冊，瀏覽課程資訊、購買課程。",
+        "An online tutoring platform for elementary and middle school students, where users can log in/register, browse course information, and purchase courses.",
       logo: ConsultlyLogo,
       link: {
         label: "OneClass Now",
@@ -128,18 +128,18 @@ export const RESUME_DATA = {
       },
     },
     {
-      title: "基金資訊觀測站",
+      title: "Fund Information Observatory",
       techStack: ["Angular", "Tailwind.css", "Highchart.js", "Nx"],
       description:
-        "政府基金資訊前後台，前台可查看基金資訊，後台有發布文章、權限管理等功能。",
+        "A government fund information system with frontend and backend functionalities. ",
       logo: MonitoLogo,
       link: {
-        label: "基金資訊觀測站",
+        label: "Fund Information Observatory",
         href: "https://www.fundclear.com.tw/home",
       },
     },
     {
-      title: "基富通官網",
+      title: "FundRich Official Website",
       techStack: [
         "Angular",
         "Tailwind",
@@ -148,10 +148,10 @@ export const RESUME_DATA = {
         "Google Analytics",
       ],
       description:
-        "全台最大專營網路基金交易平台，使用者可以登入/註冊，能夠查看基金資訊、基金盈利、並進行基金交易。",
+        "The largest online fund trading platform in Taiwan, where users can log in/register, view fund information, fund earnings, and conduct fund transactions.",
       logo: JarockiMeLogo,
       link: {
-        label: "基富通官網",
+        label: "FundRich Official Website",
         href: "https://www.fundrich.com.tw/2022OfficialWeb/",
       },
     },
